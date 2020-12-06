97582456da751599e68b47012d687b97de624efa
Sai-Adarsh <saiadarshsivakumar@gmail.com>
2020-12-06 13:41:25 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,106 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, {useMemo, useState} from 'react';
+import TinderCard from 'react-tinder-card';
+import IconButton from '@material-ui/core/IconButton';
+import WhatshotIcon from '@material-ui/icons/Whatshot';
+import ClearIcon from '@material-ui/icons/Clear';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  WhatshotIcon: {
+    '& svg': {
+      fontSize: 75,
+      color: '#fff'
+    }
+  },
+  WhatshotIconSmall: {
+    '& svg': {
+      fontSize: 20,
+      color: '#fff'
+    }
+  },
+}));
+
+const db = [
+  {
+    name: 'Richard Hendricks',
+    url: './img/richard.jpg'
+  },
+  {
+    name: 'Erlich Bachman',
+    url: './img/erlich.jpg'
+  },
+  {
+    name: 'Monica Hall',
+    url: './img/monica.jpg'
+  },
+  {
+    name: 'Jared Dunn',
+    url: './img/jared.jpg'
+  },
+  {
+    name: 'Dinesh Chugtai',
+    url: './img/dinesh.jpg'
+  }
+]
+
+const alreadyRemoved = []
+let charactersState = db 
 
 function App() {
+  const [characters, setCharacters] = useState(db)
+  const [lastDirection, setLastDirection] = useState()
+
+  const childRefs = useMemo(() => Array(db.length).fill(0).map(i => React.createRef()), [])
+  const classes = useStyles();
+  
+  const swiped = (direction, nameToDelete) => {
+    console.log('removing: ' + nameToDelete)
+    setLastDirection(direction)
+    alreadyRemoved.push(nameToDelete)
+  }
+
+  const outOfFrame = (name) => {
+    console.log(name + ' left the screen!')
+    charactersState = charactersState.filter(character => character.name !== name)
+    setCharacters(charactersState)
+  }
+
+  const swipe = (dir) => {
+    const cardsLeft = characters.filter(person => !alreadyRemoved.includes(person.name))
+    if (cardsLeft.length) {
+      const toBeRemoved = cardsLeft[cardsLeft.length - 1].name // Find the card object to be removed
+      const index = db.map(person => person.name).indexOf(toBeRemoved) // Find the index of which to make the reference to
+      alreadyRemoved.push(toBeRemoved) // Make sure the next card gets removed next time if this card do not have time to exit the screen
+      childRefs[index].current.swipe(dir) // Swipe the card!
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <IconButton className={classes.WhatshotIcon}>
+          <WhatshotIcon />
+        </IconButton>
+        <div className='cardContainer'>
+          {characters.map((character, index) =>
+            <TinderCard ref={childRefs[index]} className='swipe' key={character.name} onSwipe={(dir) => swiped(dir, character.name)} onCardLeftScreen={() => outOfFrame(character.name)}>
+              <div style={{ backgroundImage: 'url(' + character.url + ')' }} className='card'>
+                <h3>{character.name}</h3>
+              </div>
+            </TinderCard>
+          )}
+        </div>
+        <div className='buttons'>
+          <IconButton onClick={() => swipe('left')}>
+            <ClearIcon ></ClearIcon>
+          </IconButton>
+          <IconButton onClick={() => swipe('right')}>
+            <WhatshotIcon ></WhatshotIcon>
+          </IconButton>
+        </div>
+        {lastDirection ? <h2 key={lastDirection} className='infoText'>You swiped {lastDirection}</h2> : <h2 className='infoText'>Swipe a card or press a button to get started!</h2>}
       </header>
     </div>
   );
